9ef4c81f6d0c21d524bc826d27bde802c28db9e1
sokach-dev <93634186+sokach-dev@users.noreply.github.com>
2023-03-01 01:42:26 +0000

    Pending changes exported from your codespace

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+
+pub trait Retryable {
+    fn retry(&self) -> bool;
+}
+
+impl Retryable for F {
+    fn retry(&self) -> bool {
+        *self
+    }
+}
+
+struct Retry {}
+
+// a func sum a + b
+fn sum(a: i32, b: i32) -> i32 {
+    a + b
+}
+
 fn main() {
+    println!("{}", sum(1, 2));
     println!("Hello, world!");
 }
